b73cf9aa4480b3fd30df070e4fe5448dd09affbb
HerliHerviansyah <ayangherli@gmail.com>
2019-07-19 03:44:34 +0700

    herli update router/order

--- routes/order.js
+++ routes/order.js
@@ -1,1 +1,19 @@
-order.js
+const express = require('express');
+
+const router = express.Router();
+
+const orderController = require('../controllers/order');
+
+const auth = require('../configs/auth');
+
+router.get('/',auth.verifyToken, orderController.getAllOrder);
+
+router.post('/add', orderController.createOrder);
+
+router.put('/:id', orderController.updateOrder);
+
+
+//router.delete('/:id', OrderController.deleteProduct);
+
+
+module.exports = router;
